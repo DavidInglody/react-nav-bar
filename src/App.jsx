f42f4a7034460a095216fa9a85afb0091918ff10
David Inglody <inglody96@gmail.com>
2023-10-10 14:30:30 +0200

    navbar

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import {links,social} from "./data"
+import {FaBars} from "react-icons/fa"
+import netflix from "./images/netflix.png"
+import { useState ,useRef} from "react";
+
+const App = () => {
+  const[showLinks, setShowLinks]= useState(false)
+  const linksContainerRef = useRef(null)
+  const linksRef = useRef(null)
+
+  const toggleLinks =() =>{
+    setShowLinks(!showLinks)
+  }
+
+  const linkStyles = {
+    height: showLinks ? `${linksRef.current.getBoundingClientRect().height}px` : "0px",
+  }
+
+  return <nav>
+    <div className="nav-center">
+      <div className="nav-header">
+        <img src={netflix} className="logo" style={{width:"100px"}} alt="logo" />
+        <button className="nav-toggle" onClick={toggleLinks}><FaBars/></button>
+      </div>
+
+      <div 
+        className= "links-container" ref={linksContainerRef} style={linkStyles}>
+        <ul className="links" ref= {linksRef}>
+          {links.map((link)=>{
+            const {id,url,text} = link
+            return <li key={id}>
+              <a href={url}>{text}</a>
+            </li>
+          })}
+        </ul>
+      </div>
+
+      <ul className="social-icons">
+        {social.map((link)=>{
+          const {id,url,icon} = link
+          return <li key={id}>
+            <a href={url} target="_blank">{icon}</a>
+            </li>
+        })}
+      </ul>
+    </div>
+
+  </nav> 
+};
+export default App;
